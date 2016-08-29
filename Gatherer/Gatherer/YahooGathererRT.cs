a5681a946a82ff8ffc3903fb3cbfe40429063869
Dor Shwartz <dor.shwartz@campus.technion.ac.il>
2016-08-29 08:03:02 +0300

    eytan says im missing a file

---
+++ Gatherer/Gatherer/YahooGathererRT.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Timers;
+using Stock = System.String;
+using CsvHelper;
+using Gatherer;
+
+namespace Gatherers
+{
+
+    public class YahooGathererRT : YahooGatherer
+    {
+        public YahooGathererRT(int updatePeriod, IEnumerable<string> enumerable, params string[] args) : base(updatePeriod, enumerable, args)
+        {
+        }
+
+        protected override ICollection<string> GetModifiers()
+        {
+            return Enum.GetValues(typeof(DataModifiers)).Cast<string>().ToList<string>();
+        }
+
+        public enum DataModifiers
+        {
+            [Description("ask")]
+            b2,
+
+            [Description("bid")]
+            b3,
+
+            [Description("change")]
+            c6,
+
+            [Description("holdings gain percent")]
+            g5,
+
+            [Description("holdings gain")]
+            g6,
+
+            [Description("order book")]
+            i5,
+
+            [Description("market cap")]
+            j3,
+
+            [Description("last trade")]
+            k1,
+
+            [Description("change percent")]
+            k2,
+
+            [Description("day's range")]
+            m2,
+
+            [Description("p/e ratio")]
+            r2,
+
+            [Description("holdings value")]
+            v7,
+
+            [Description("day's value change")]
+            w4
+        }
+    }  
+}
