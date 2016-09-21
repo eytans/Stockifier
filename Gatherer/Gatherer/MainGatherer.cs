c5f14c607e2638e0579df740b6ab523adae42eda
Eytan Singher <eytan.singher@gmail.com>
2016-09-22 02:15:40 +0300

    Main Gathering: Done. waiting on Add then starting some testing
    Signed-off-by: Eytan Singher <eytan.singher@gmail.com>

--- Gatherer/Gatherer/MainGatherer.cs
+++ Gatherer/Gatherer/MainGatherer.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Gatherer
@@ -11,20 +12,20 @@ namespace Gatherer
     class MainGatherer
     {
         protected static Logger logger = LogManager.GetCurrentClassLogger();
+        public static ManualResetEvent waitOn = new ManualResetEvent(false);
+        private IDictionary<string, StockContext> stocks;
 
-        private IList<StockContext> stocks;
-
-        MainGatherer(IList<string> stocks)
+        MainGatherer(ICollection<string> stocks)
         {
             logger.Info("Connecting to all stocks databases.");
-            this.stocks = new List<StockContext>();
+            this.stocks = new Dictionary<string, StockContext>();
             foreach (string s in stocks)
             {
                 logger.Debug("Connecting to " + s);
                 try
                 {
                     var temp = new StockContext(s);
-                    this.stocks.Add(temp);
+                    this.stocks.Add(s, temp);
                 }
                 catch(Exception e)
                 {
@@ -39,16 +40,38 @@ namespace Gatherer
             
         }
 
+        private void AddToDb<T>(object sender, DataUpdatedArgs args, Func<IDictionary<string, string>, T> creator)
+        {
+            var stocksValues = args.Values;
+            foreach (var stockAnsiToValues in stocksValues)
+            {
+                T stock = creator(stockAnsiToValues.Value);
+                // TODO: @Dor I need an add function for StockContext with overload for both stock types.
+                this.stocks[stockAnsiToValues.Key].Add(stock);
+            }
+        }
+
+        private void AddRTToDB(object sender, DataUpdatedArgs args)
+        {
+            AddToDb<StockRT>(sender, args, (IDictionary<string, string> data) => new StockRT(data));
+        }
+
+        private void AddDailyToDB(object sender, DataUpdatedArgs args)
+        {
+            AddToDb<StockDaily>(sender, args, (IDictionary<string, string> data) => new StockDaily(data));
+        }
+
         public void Run()
         {
-            string[] stockNames = stocks.Select((StockContext sc) => sc.StockName).ToArray();
+            string[] stockNames = stocks.Keys.ToArray();
             int dayInMilliseconds = 1000 * 60 * 60 * 24;
             int fiveMinutesInMilliseconds = 1000 * 5;
             logger.Info("Creating daily gatherer");
             YahooGathererDaily daily = new YahooGathererDaily(dayInMilliseconds, stockNames);
             logger.Info("Creating realtime gatherer which will sample every 5 minutes");
             YahooGathererRT rt = new YahooGathererRT(fiveMinutesInMilliseconds, stockNames);
-            // TODO: add a function to DataUpdated event to read all data into database
+            rt.DataUpdated += AddRTToDB;
+            daily.DataUpdated += AddDailyToDB;
         }
 
         public static void Main(string[] args)
@@ -65,7 +88,7 @@ namespace Gatherer
             }
 
             MainGatherer gatherer = new MainGatherer(stockNames);
-            // TODO: dont return so data will be collected continuesly.
+            waitOn.WaitOne();
         }
     }
 }
