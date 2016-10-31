0725fb6f18dae84ffc1f3fcf457d8872d7a3cf15
Eytan Singher <eytan.singher@gmail.com>
2016-11-01 00:17:57 +0200

    Classifiers: added the start of classifiers project

---
+++ Classifiers/Classifiers/TestingClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Accord.MachineLearning.Boosting;
+using Accord.MachineLearning.DecisionTrees;
+using Accord.MachineLearning.Bayes;
+
+namespace Classifiers
+{
+    public class TestingClass
+    {
+        DecisionTree t = new DecisionTree(null, 2);
+        AdaBoost<NaiveBayes> booster;
+    }
+}
