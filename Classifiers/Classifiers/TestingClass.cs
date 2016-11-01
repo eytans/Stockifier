962997bddf468685dbc56312ffbe2277183a8c73
Eytan Singher <eytan.singher@gmail.com>
2016-11-02 01:41:05 +0200

    Classifier: still working on tree api

--- Classifiers/Classifiers/TestingClass.cs
+++ Classifiers/Classifiers/TestingClass.cs
@@ -9,6 +9,8 @@ using Accord.MachineLearning.DecisionTrees;
 using Accord.IO;
 using Accord.MachineLearning.Bayes;
 using System.Data;
+using Accord.Statistics.Filters;
+using Accord.MachineLearning.DecisionTrees.Learning;
 
 namespace Classifiers
 {
@@ -16,20 +18,81 @@ namespace Classifiers
     {
         public TestingClass()
         {
-            string dataPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "Samples", "Accident & Health Insurance (Financial)", "AFL.csv");
+            string dataPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+                "..", "..", "..", "Samples", "adult", "Dataset.data");
+            //    "..", "..", "..", "Samples", "Accident & Health Insurance (Financial)", "AFL.csv");
             CsvReader reader = new CsvReader(dataPath, false);
             var stringTable = reader.ToTable();
-            var table = stringTable.Clone();
-            for(int i = 2; i < table.Columns.Count; i++)
-                table.Columns[i].DataType = typeof(double);
+            System.Data.DataTable tempTable = stringTable.Clone();
+
+            //tempTable.Columns[0].DataType = typeof(int);
+            //tempTable.Columns[1].DataType = typeof(int);
+            //for (int i = 2; i < tempTable.Columns.Count; i++)
+            //    tempTable.Columns[i].DataType = typeof(double);
+
+            //string[] names = {"ticker", "date", "open", "high", "low",
+            //    "close", "volume", "ex-divider", "split-ratio", "adj-open", "adj-high",
+            //    "adj-low", "adj-close", "adj-volume"};
+            //for (int i = 0; i < tempTable.Columns.Count; i++)
+            //    tempTable.Columns[i].ColumnName = names[i];
+
+            //foreach (DataRow row in stringTable.Rows)
+            //    tempTable.ImportRow(row);
+
+            //var filter = new Codification(tempTable, "ticker", "date");
+            //var table = filter.Apply(tempTable);
+
+            for (int i = 0; i < tempTable.Columns.Count; i++)
+                tempTable.Columns[i].DataType = typeof(int);
+
+
+            string[] names = {"age", "workclass", "fnlwgt", "education", "educational-num",
+                "marital-status", "occupation", "relationship", "race", "gender",
+                "capital-gain", "capital-loss", "hours-per-week", "native-country", "income"};
+            int[] valuesRange = {Int32.MaxValue, 8, Int32.MaxValue, 16, Int32.MaxValue, 7,
+                14, 6, 5, 2, Int32.MaxValue, Int32.MaxValue, 169, 40, 2};
+
+            for (int i = 0; i < tempTable.Columns.Count; i++)
+                tempTable.Columns[i].ColumnName = names[i];
 
             foreach (DataRow row in stringTable.Rows)
-                table.ImportRow(row);
+                tempTable.ImportRow(row);
 
             stringTable.Dispose();
+            tempTable.Dispose();
 
-            DecisionTree t = new DecisionTree(null, 2);
-            AdaBoost<NaiveBayes> booster;
+            var filter = new Codification(tempTable, "age", "workclass", "fnlwgt", "education", "educational-num",
+                "marital-status", "occupation", "relationship", "race", "gender",
+                "capital-gain", "capital-loss", "hours-per-week", "native-country", "income");
+            var table = filter.Apply(tempTable, "age", "workclass", "fnlwgt", "education", "educational-num",
+                "marital-status", "occupation", "relationship", "race", "gender",
+                "capital-gain", "capital-loss", "hours-per-week", "native-country", "income");
+
+            IList<DecisionVariable> attributes = new List<DecisionVariable>();
+            for(int i = 0; i < names.Count() - 1; i++)
+                attributes.Add(DecisionVariable.Discrete(names[i], valuesRange[i]));
+
+            //foreach (string name in names.ToList().GetRange(0, 2))
+            //    attributes.Add(DecisionVariable.Discrete(name, 16));
+            //foreach (string name in names.ToList().GetRange(2, names.Count() - 2))
+            //    attributes.Add(DecisionVariable.Continuous(name));
+
+            DataTable outputs = table.Clone();
+            for (int i = 0; i < outputs.Columns.Count; i++)
+                outputs.Columns.RemoveAt(i);
+
+            DataTable inputs = table.Clone();
+            inputs.Columns.RemoveAt(14);
+
+            inputs.Rows.Cast<DataRow>().Select(r => r.ItemArray).ToArray();
+
+            DecisionTree t = new DecisionTree(attributes, 2);
+            ID3Learning id3learning = new ID3Learning(t);
+            id3learning.Run(inputs., 
+                outputs);
+            id3learning.Save("out.tree");
+
+            //AdaBoost<NaiveBayes> booster;
         }
         
     }
