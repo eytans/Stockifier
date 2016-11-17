03a7d5c0825d9a91dc2c8971234215e33f2b1c99
Eytan Singher <eytan.singher@gmail.com>
2016-11-18 00:06:43 +0200

    Classifiers: we can classify shit!
    Signed-off-by: Eytan Singher <eytan.singher@gmail.com>

--- Classifiers/classifiers.py
+++ Classifiers/classifiers.py
@@ -1,6 +1,28 @@
 import sklearn.ensemble
-from sklearn.model_selection import cross_val_score
+from itertools import tee
+from collections import OrderedDict
 
 
-def get_general_classifier(data, classes):
+def __get_general_classifier(data, classes):
     return sklearn.ensemble.AdaBoostClassifier().fit(data, classes)
+
+
+def apply_func(func, iterable):
+    for i in iterable:
+        func(i)
+        yield i
+
+
+def get_data_and_results_from_stock(stock_data):
+    data = list(map(lambda d: OrderedDict(sorted(d.items())), stock_data))
+    data.sort(key=lambda r: r['intdate'])
+    data = list(data)
+    results = list(map(lambda d: (d['close'] - d['open']) * d['open'] < 0.3, data[1:]))
+    data = list(map(lambda d: list(d.values()), data))
+    data = data[:-1]
+    return data, results
+
+
+def train_stock(stock_data):
+    data, results = get_data_and_results_from_stock(stock_data)
+    return __get_general_classifier(data, results)
