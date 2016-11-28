ccbb075c077775eaf7cfce08367122998fd058ae
Eytan Singher <eytan.singher@gmail.com>
2016-11-29 00:40:48 +0200

    Fixed add_stock_attributes
    Signed-off-by: Eytan Singher <eytan.singher@gmail.com>

--- Utilities/add_stock_attributes.py
+++ Utilities/add_stock_attributes.py
@@ -18,18 +18,19 @@ def main():
     learning_data = LearningData()
     market_data = learning_data.get_market_data()
     stock_data = learning_data.get_stock_data()
-    for stock_name, data in stock_data:
-        for date, doc in data:
+    for stock_name, data in stock_data.items():
+        for date, doc in data.items():
+            update = {}
             for j in range(1, 1 + args.daymemory):
                 new_date = date - datetime.timedelta(days=j)
-                doc[str(j) + '_days_before'] = None
+                update[str(j) + '_days_before'] = None
                 if new_date in data:
-                    doc[str(j) + '_days_before'] = data[new_date]['_id']
+                    update[str(j) + '_days_before'] = data[new_date]['_id']
                 for market_name in market_data.keys():
-                    doc['markets_' + str(j) + '_days_before'] = None
+                    update['markets_' + str(j) + '_days_before'] = {market_name: None}
                     if new_date in market_data[market_name]:
-                        doc['markets_' + str(j) + '_days_before'] = market_data[market_name][new_date]['_id']
-            cl.update_one({'_id': doc['_id']})
+                        update['markets_' + str(j) + '_days_before'] = {market_name: market_data[market_name][new_date]['_id']}
+            cl.update_one(filter={'_id': doc['_id']}, update={'$set': update})
 
 
 if __name__ == '__main__':
