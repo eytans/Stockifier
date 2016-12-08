bfda6c1fa2711651b821328045565690d57e2473
eytan <eytan.singher@gmail.com>
2016-12-08 17:50:44 +0200

    Utilities: fixing bug in creating market history. updating pymongo inner object need dot notation
    Signed-off-by: eytan <eytan.singher@gmail.com>

--- Utilities/__init__.py
+++ Utilities/__init__.py
@@ -2,3 +2,12 @@ import os
 project_dir = os.path.dirname(os.path.dirname(__file__))
 utilities_dir = os.path.dirname(__file__)
 default_pickle = os.path.join(project_dir, 'learning_data.pickle')
+
+
+def stock_history_field(day, field=''):
+    return field+str(day)+'_days_before'
+
+
+def market_history_field(day, field=''):
+    return field+'markets_'+str(day)+'_days_before'
+
