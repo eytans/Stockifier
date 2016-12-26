8fa5c32ffc6a7c3e3ccf41f1733cb821635d5882
Eytan Singher <eytan.singher@gmail.com>
2016-12-26 17:07:06 +0200

    Classifiers: fixed test to not have repeating values
    Signed-off-by: Eytan Singher <eytan.singher@gmail.com>

--- Classifiers/test_classifiers.py
+++ Classifiers/test_classifiers.py
@@ -20,19 +20,20 @@ class Test_classifiers(TestCase):
         self.assertIsNotNone(clf)
 
     def test_adaboost_acc(self):
-        stock_name = 'GMED'
-        group_kfold = KFold(n_splits=5)
+        stock_name = 'ABC'
+        group_kfold = KFold(n_splits=5, shuffle=True)
         accuracies = []
         clf, data, classes = create_adaboost(stock_name, return_data=True)
-        for train_index, test_index in group_kfold.split(data):
-            clf = create_adaboost(stock_name, data=data.iloc[train_index[0]:train_index[-1]].drop('change', axis=1))
-            temp = data.iloc[test_index[0]:test_index[-1]].drop('change', axis=1)
-            temp_c = classes[test_index[0]:test_index[-1]]
+        for train_indices, test_indices in group_kfold.split(data):
+            train_data = data.loc[data.index[train_indices]].drop('change', axis=1)
+            clf = create_adaboost(stock_name, data=train_data)
+            temp = data.loc[data.index[test_indices]].drop('change', axis=1)
+            temp_c = classes.loc[data.index[test_indices]]
             count = 0
             for d, c in zip(temp.iterrows(), temp_c):
                 count += clf.predict(d[1]) == c
-            accuracies.append(float(count)/len(temp_c))
+            accuracies.append(float(count)/len(test_indices))
         acc = sum(accuracies)/len(accuracies)
-        print(acc)
+        print("avarage {} accuracy for {} runs".format(acc, len(accuracies)))
         self.assertGreaterEqual(acc, 0.75)
 
